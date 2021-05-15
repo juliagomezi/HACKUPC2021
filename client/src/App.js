b703f7ed52597af9d12894e6664629f8063da2f1
Júlia <julia.gomez.i@estudiantat.upc.edu>
2021-05-15 11:26:07 +0200

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -5,17 +5,6 @@ import './App.css';
 import react from 'react';
 import Main from './Main.js';
 
-const { MongoClient } = require("mongodb");
-const Express = require("express")
-
- 
-// Replace the following with your Atlas connection string
-const url = "mongodb+srv://admin:admin1234@cluster0.vgm2t.mongodb.net/hackupc2021?retryWrites=true&w=majority";
-const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
- 
- // The database to use
- const dbName = "hackupc2021";
-
 class App extends Component {
 
 	render() {
--- client/src/Main.js
+++ client/src/Main.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
 
+import './Main.css';
+
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
 class Main extends Component {
 
 	render() {
 		return (
-			<div >Hola
+			<div>
+				<Card className="root">
+					<CardContent>
+						<Typography className="title" color="textSecondary" gutterBottom>
+							Word of the Day
+						</Typography>
+					</CardContent>
+					<CardActions>
+						<Button size="small">Learn More</Button>
+					</CardActions>
+				</Card>
 			</div>
-
 		);
 	}
 }
